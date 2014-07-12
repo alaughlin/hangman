0a53ada67e93a69bae0cfc612801b77898c16414
alaughlin <adamlaughlin@outlook.com>
2014-07-12 09:21:48 -0400

    refactored prompt. also computerplayer can intelligently guess next letter based on letter popularity

--- hangman.rb
+++ hangman.rb
@@ -80,11 +80,8 @@ class HumanPlayer
     valid_letter = false
 
     until valid_letter
-      puts "Turns_left: #{chances_left}"
-      puts "Current word: #{current_word.join(" ")}"
-      puts "Guessed letters: #{@guessed_letters.sort.join(" ")}\n\n"
       valid_letter = true
-      print "Guess a letter: "
+      prompt_text(current_word, chances_left)
       letter = gets.chomp
 
       if @guessed_letters.include?(letter)
@@ -101,12 +98,20 @@ class HumanPlayer
 
     letter
   end
+
+  def prompt_text(current_word, chances_left)
+    puts "Turns_left: #{chances_left}"
+    puts "Current word: #{current_word.join(" ")}"
+    puts "Guessed letters: #{@guessed_letters.sort.join(" ")}\n\n"
+    print "Guess a letter: "
+  end
 end
 
 class ComputerPlayer
   def initialize
     @dictionary = File.readlines("dictionary.txt").map(&:chomp)
-    @shorten_list_by_length = false
+    @shorten_choices_by_length = false
+    @guessed_letters = []
   end
 
   def choose_word
@@ -126,14 +131,38 @@ class ComputerPlayer
   end
 
   def guess_letter(current_word, chances_left)
-    first_minimize(current_word) if !@shorten_list_by_length
+    prompt_text(current_word, chances_left)
+    first_minimize(current_word) if !@shorten_choices_by_length
 
-    
+    letter = get_most_popular_letter
+
+    letter
   end
 
   def first_minimize(current_word)
     @dictionary = @dictionary.select { |word| word.length == current_word.length }
-    @shorten_list_by_length = true
+    @shorten_choices_by_length = true
+  end
+
+  def get_most_popular_letter
+    letter_counts = Hash.new(0)
+
+    @dictionary.each do |word|
+      word.each_char do |letter|
+        letter_counts[letter] += 1 unless @guessed_letters.include?(letter)
+      end
+    end
+
+    popular_letter = letter_counts.sort_by { |key, val| val }.reverse[0][0]
+    @guessed_letters << popular_letter
+
+    popular_letter
+  end
+
+  def prompt_text(current_word, chances_left)
+    puts "Turns_left: #{chances_left}"
+    puts "Current word: #{current_word.join(" ")}"
+    puts "Guessed letters: #{@guessed_letters.sort.join(" ")}\n\n"
   end
 end
 
