f2df000a2b40b6fd42239eb8aef245e5a50f0892
alaughlin <adamlaughlin@outlook.com>
2014-07-12 08:02:04 -0400

    added interfaces to classes

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,41 @@
 class Game
+  def initialize(player1, player2)
+    # player1 is checker, player2 is guesser
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def play
+  end
+
+  def won?
+  end
 end
 
 class HumanPlayer
+  def initialize
+  end
+
+  def choose_word
+  end
+
+  def check_letter
+  end
+
+  def guess_letter
+  end
 end
 
 class ComputerPlayer
+  def initialize
+  end
+
+  def choose_word
+  end
+
+  def check_letter
+  end
+
+  def guess_letter
+  end
 end
